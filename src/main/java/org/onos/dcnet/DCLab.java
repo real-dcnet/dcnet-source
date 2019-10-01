38e7aa6ac6fd7172b553db8c15500eb62fb223cf
JordanR159 <reed226@purdue.edu>
2019-10-01 01:08:53 -0400

    Make changes to log response from switch

--- src/main/java/org/onos/dcnet/DCLab.java
+++ src/main/java/org/onos/dcnet/DCLab.java
@@ -1,6 +1,8 @@
 package org.onos.dcnet;
 
 import org.onosproject.net.Device;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -8,6 +10,9 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 public class DCLab {
+    /** Logs information, errors, and warnings during runtime. */
+    private static Logger log = LoggerFactory.getLogger(DCLab.class);
+
     public static void configureSwitch(final Device device) {
         Client client = ClientBuilder.newClient();
         Response response = client
@@ -20,6 +25,7 @@ public class DCLab {
                 .accept(MediaType.APPLICATION_JSON)
                 .get();
         String token = response.getEntity().toString();
-        System.out.println(token);
+        log.info(Integer.toString(response.getStatus()));
+        log.info(token);
     }
 }
