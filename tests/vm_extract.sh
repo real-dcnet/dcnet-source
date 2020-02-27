d884cbc01f1e95c6980820a6b0932f1f7a4a6836
William Reed <reed226@purdue.edu>
2020-02-27 04:03:54 +0000

    Transfer ping results from VM during migration

--- tests/vm_extract.sh
+++ tests/vm_extract.sh
@@ -2,8 +2,9 @@
 
 TEST_DIRS=("lf5-lf5" "lf5-lf6" "lf5-lf3" "lf5-lf8" "lf5-lf14")
 
-for dir in $TEST_DIRS
+for dir in ${TEST_DIRS[@]}
 do
+	echo $dir
 	for i in $(seq 1 10)
 	do
 		scp "dcnet@128.10.126.57:$dir/run$i/migrate_test_vm.out" "$1/$dir/run$i"
