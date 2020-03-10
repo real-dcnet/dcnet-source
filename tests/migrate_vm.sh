50e3ddbf5a0e11210663992d7a1a735b3bc9d90a
William Reed <reed226@purdue.edu>
2020-03-10 05:00:11 +0000

    Add migration results with bandwidth limit

--- tests/migrate_vm.sh
+++ tests/migrate_vm.sh
@@ -5,6 +5,6 @@
 # Arg 3: Migration connection destination
 # Arg 4: VM Address
 
-virsh -c qemu+ssh://dcnet@$1/system migrate-setspeed ubuntu 30
+virsh -c qemu+ssh://dcnet@$1/system migrate-setspeed ubuntu 20
 virsh -c qemu+ssh://dcnet@$1/system migrate ubuntu qemu+ssh://dcnet@$2/system tcp://$3 --live --verbose
 echo "$3/32:$4/32:" > /dev/udp/10.0.1.8/10018
--- tests/vm_test.sh
+++ tests/vm_test.sh
@@ -15,14 +15,16 @@ do
 	ssh -o StrictHostKeyChecking=no dcnet@128.10.126.57 \
 		"echo \"--- Ping during migration from $1 to $2, address $4 ---\" > $VM_FILE"
 	sleep 1
-	ssh -o StrictHostKeyChecking=no dcnet@128.10.126.57 "ping -c 80 $3 >> $VM_FILE &"
+	ssh -o StrictHostKeyChecking=no dcnet@128.10.126.57 "ping -c 100 $3 >> $VM_FILE &"
+	ssh -o StrictHostKeyChecking=no dcnet@$1 "ping -c 100 $4 > /dev/null &"
+	ssh -o StrictHostKeyChecking=no dcnet@$2 "ping -c 100 $3 > /dev/null &"
 	mkdir -p "$5/run$i"
 	echo "--- Ping during migration from $1 to $2, address $4 ---" > $HV_FILE
-	ping -c 80 128.10.126.57 >> $HV_FILE &
+	ping -c 100 128.10.126.57 >> $HV_FILE &
 	sleep 20
 	echo "Migration Emminent"
 	source migrate_vm.sh $1 $2 $4 "128.10.126.57"
-	sleep 60
+	sleep 80
 
 
 	ssh -o StrictHostKeyChecking=no dcnet@128.10.126.57 "echo \"\" >> $VM_FILE"
@@ -30,12 +32,14 @@ do
 	ssh -o StrictHostKeyChecking=no dcnet@128.10.126.57 \
 		"echo \"--- Ping during migration from $2 to $1, address $3 ---\" >> $VM_FILE"
 	sleep 1
-	ssh -o StrictHostKeyChecking=no dcnet@128.10.126.57 "ping -c 80 $3 >> $VM_FILE &"
+	ssh -o StrictHostKeyChecking=no dcnet@128.10.126.57 "ping -c 100 $3 >> $VM_FILE &"
 	echo "" >> $HV_FILE 
 	echo "--- Ping during migration from $2 to $1, address $3 ---" >> $HV_FILE
-	ping -c 80 128.10.126.57 >> $HV_FILE &
+	ssh -o StrictHostKeyChecking=no dcnet@$1 "ping -c 100 $4 > /dev/null &"
+	ssh -o StrictHostKeyChecking=no dcnet@$2 "ping -c 100 $3 > /dev/null &"
+	ping -c 100 128.10.126.57 >> $HV_FILE &
 	sleep 20
 	echo "Migration Emminent"
 	source migrate_vm.sh $2 $1 $3 "128.10.126.57"
-	sleep 60
+	sleep 80
 done
