e0ceb7d00ee1a2078e49bebe56b6ef7f2102d818
William Reed <reed226@purdue.edu>
2020-03-11 15:56:53 +0000

    Parse parameters for TCP tests

--- tests/run_tests_tcp.sh
+++ tests/run_tests_tcp.sh
@@ -8,10 +8,10 @@ do
 	mkdir run$i
 	while IFS= read -r line
 	do
-		echo $line
 		loc=$(echo $line | awk '{print $1}')
-		echo $loc
-		#echo "Testing bandwidth from $SRC_IP to ${DST_IPS[$j]}"
-		#source "./tcp_test.sh" "$SRC_IP" "${DST_IPS[$j]}" "run$i/tcp_test.out"
+		src=$(echo $line | awk '{print $2}')
+		dst=$(echo $line | awk '{print $3}')
+		echo "Testing bandwidth from $SRC_IP to ${DST_IPS[$j]}"
+		source "./tcp_test.sh" "$src" "$dst" "run$i/"$loc"_tcp.out"
 	done < "$input"
 done
