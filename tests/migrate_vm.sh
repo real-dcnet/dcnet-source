ada5beb37108252bda11b4309fb82d21bfe4a5cd
William Reed <reed226@purdue.edu>
2020-06-21 16:35:13 +0000

    Make migration script more readable

--- tests/migrate_vm.sh
+++ tests/migrate_vm.sh
@@ -5,6 +5,11 @@
 # Arg 3: Migration connection destination
 # Arg 4: VM Address
 
-virsh -c qemu+ssh://dcnet@$1/system migrate-setspeed ubuntu 20
+virsh -c qemu+ssh://dcnet@$1/system migrate-setspeed ubuntu 500
 virsh -c qemu+ssh://dcnet@$1/system migrate ubuntu qemu+ssh://dcnet@$2/system tcp://$3 --live --verbose
-echo "$3/32:$4/32:" > /dev/udp/10.0.1.8/10018
+
+# Uncomment this line for DCnet migration
+#echo "$3/32:$4/32:" > /dev/udp/10.0.1.8/10018
+
+# Uncomment this line for reactive forwarding migration
+echo "reactive:$4/32:" > /dev/udp/10.0.1.8/10018
--- tests/vm_test.sh
+++ tests/vm_test.sh
@@ -6,6 +6,8 @@
 # Arg 4: Migration connection source
 # Arg 5: Directory to store runs
 
+count=80
+
 for i in $(seq 1 10)
 do
 	VM_FILE="$5/run$i/migrate_test_vm.out"
@@ -15,16 +17,16 @@ do
 	ssh -o StrictHostKeyChecking=no dcnet@128.10.126.57 \
 		"echo \"--- Ping during migration from $1 to $2, address $4 ---\" > $VM_FILE"
 	sleep 1
-	ssh -o StrictHostKeyChecking=no dcnet@128.10.126.57 "ping -c 100 $3 >> $VM_FILE &"
-	ssh -o StrictHostKeyChecking=no dcnet@$1 "ping -c 100 $4 > /dev/null &"
-	ssh -o StrictHostKeyChecking=no dcnet@$2 "ping -c 100 $3 > /dev/null &"
+	ssh -o StrictHostKeyChecking=no dcnet@128.10.126.57 "ping -c $count $3 >> $VM_FILE &"
+	ssh -o StrictHostKeyChecking=no dcnet@$1 "ping -c $count $4 > /dev/null &"
+	ssh -o StrictHostKeyChecking=no dcnet@$2 "ping -c $count $3 > /dev/null &"
 	mkdir -p "$5/run$i"
 	echo "--- Ping during migration from $1 to $2, address $4 ---" > $HV_FILE
-	ping -c 100 128.10.126.57 >> $HV_FILE &
+	ping -c $count 128.10.126.57 >> $HV_FILE &
 	sleep 20
 	echo "Migration Emminent"
 	source migrate_vm.sh $1 $2 $4 "128.10.126.57"
-	sleep 80
+	sleep $(($count - 20))
 
 
 	ssh -o StrictHostKeyChecking=no dcnet@128.10.126.57 "echo \"\" >> $VM_FILE"
@@ -32,14 +34,14 @@ do
 	ssh -o StrictHostKeyChecking=no dcnet@128.10.126.57 \
 		"echo \"--- Ping during migration from $2 to $1, address $3 ---\" >> $VM_FILE"
 	sleep 1
-	ssh -o StrictHostKeyChecking=no dcnet@128.10.126.57 "ping -c 100 $3 >> $VM_FILE &"
+	ssh -o StrictHostKeyChecking=no dcnet@128.10.126.57 "ping -c $count $3 >> $VM_FILE &"
 	echo "" >> $HV_FILE 
 	echo "--- Ping during migration from $2 to $1, address $3 ---" >> $HV_FILE
-	ssh -o StrictHostKeyChecking=no dcnet@$1 "ping -c 100 $4 > /dev/null &"
-	ssh -o StrictHostKeyChecking=no dcnet@$2 "ping -c 100 $3 > /dev/null &"
-	ping -c 100 128.10.126.57 >> $HV_FILE &
+	ssh -o StrictHostKeyChecking=no dcnet@$1 "ping -c $count $4 > /dev/null &"
+	ssh -o StrictHostKeyChecking=no dcnet@$2 "ping -c $count $3 > /dev/null &"
+	ping -c $count 128.10.126.57 >> $HV_FILE &
 	sleep 20
 	echo "Migration Emminent"
 	source migrate_vm.sh $2 $1 $3 "128.10.126.57"
-	sleep 80
+	sleep $(($count - 20))
 done
